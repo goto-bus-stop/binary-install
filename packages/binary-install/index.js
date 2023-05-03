55bf77a1286733ce39e76a2aa6b66071a3b9008e
Ronan Sandford <wighawag@gmail.com>
2023-05-03 23:29:00 +0100

    ability to specify install dir (#29)
    Co-authored-by: Avery Harnish <averyharnish@gmail.com>

--- packages/binary-install/index.js
+++ packages/binary-install/index.js
@@ -12,7 +12,7 @@ const error = msg => {
 };
 
 class Binary {
-  constructor(name, url) {
+  constructor(name, url, config) {
     let errors = [];
     if (typeof url !== "string") {
       errors.push("url must be a string");
@@ -42,7 +42,7 @@ class Binary {
     }
     this.url = url;
     this.name = name;
-    this.installDirectory = join(__dirname, "node_modules", ".bin");
+    this.installDirectory = config?.installDirectory || join(__dirname, "node_modules", ".bin");
 
     if (!existsSync(this.installDirectory)) {
       mkdirSync(this.installDirectory, { recursive: true });
